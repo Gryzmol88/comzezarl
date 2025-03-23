0492b16a77b83ac769a5bfe060c6eda8ac05e7d8
Michu <mnadrajkowski@wp.pl>
2025-03-23 20:33:11 +0100

    Zmienieno settings

--- comzezarl/settings.py
+++ comzezarl/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = 'django-insecure-9%0tq=c)lsfxp1(6_#e4z(kik9q$1fhsl5h@36^sr@(@%ks5#1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+
 ALLOWED_HOSTS = ["nadramic.pythonanywhere.com", "127.0.0.1"]
 
 
