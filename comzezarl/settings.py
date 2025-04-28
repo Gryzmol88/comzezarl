4674baa27e8c8878cdc8093aba9966e2eee607d4
Michu <mnadrajkowski@wp.pl>
2025-04-28 20:50:59 +0200

    Dodano obsługe nowych postów

--- comzezarl/settings.py
+++ comzezarl/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9%0tq=c)lsfxp1(6_#e4z(kik9q$1fhsl5h@36^sr@(@%ks5#1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ["nadramic.pythonanywhere.com", "127.0.0.1"]
